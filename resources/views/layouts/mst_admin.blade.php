8824f18a74dfb7fb948ec706bc322d1c1cdb997f
Fiqy Ainuzzaqy <fiqy_a@icloud.com>
2018-10-10 23:14:40 +0700

    minor update on admin page - sidebar navigation

--- resources/views/layouts/mst_admin.blade.php
+++ resources/views/layouts/mst_admin.blade.php
@@ -30,7 +30,7 @@
     </style>
 </head>
 
-<body class="nav-md footer_fixed">
+<body class="nav-md">
 @php $feedback = \App\Feedback::where('created_at', '>=', today()->subWeek()->toDateTimeString()); @endphp
 <div class="container body">
     <div class="main_container">
@@ -63,104 +63,76 @@
                     <div class="menu_section">
                         <h3>General</h3>
                         <ul class="nav side-menu">
-                            <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                            <li><a><i class="fa fa-home"></i> Home
+                                    <span class="fa fa-chevron-down"></span></a>
                                 <ul class="nav child_menu">
-                                    <li><a href="index.html">Dashboard</a></li>
-                                    <li><a href="index2.html">Dashboard2</a></li>
-                                    <li><a href="index3.html">Dashboard3</a></li>
+                                    <li><a href="{{route('home-admin')}}">Dashboard</a></li>
+                                    <li><a href="{{route('admin.inbox')}}">Inbox</a></li>
                                 </ul>
                             </li>
-                            <li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
+                            <li><a><i class="fa fa-table"></i> Tables
+                                    <span class="fa fa-chevron-down"></span></a>
                                 <ul class="nav child_menu">
-                                    <li><a href="form.html">General Form</a></li>
-                                    <li><a href="form_advanced.html">Advanced Components</a></li>
-                                    <li><a href="form_validation.html">Form Validation</a></li>
-                                    <li><a href="form_wizards.html">Form Wizard</a></li>
-                                    <li><a href="form_upload.html">Form Upload</a></li>
-                                    <li><a href="form_buttons.html">Form Buttons</a></li>
-                                </ul>
-                            </li>
-                            <li><a><i class="fa fa-desktop"></i> UI Elements <span
-                                            class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu">
-                                    <li><a href="general_elements.html">General Elements</a></li>
-                                    <li><a href="media_gallery.html">Media Gallery</a></li>
-                                    <li><a href="typography.html">Typography</a></li>
-                                    <li><a href="icons.html">Icons</a></li>
-                                    <li><a href="glyphicons.html">Glyphicons</a></li>
-                                    <li><a href="widgets.html">Widgets</a></li>
-                                    <li><a href="invoice.html">Invoice</a></li>
-                                    <li><a href="inbox.html">Inbox</a></li>
-                                    <li><a href="calendar.html">Calendar</a></li>
-                                </ul>
-                            </li>
-                            <li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu">
-                                    <li><a href="tables.html">Tables</a></li>
-                                    <li><a href="tables_dynamic.html">Table Dynamic</a></li>
-                                </ul>
-                            </li>
-                            <li><a><i class="fa fa-chart-bar"></i> Data Presentation <span
-                                            class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu">
-                                    <li><a href="chartjs.html">Chart JS</a></li>
-                                    <li><a href="chartjs2.html">Chart JS2</a></li>
-                                    <li><a href="morisjs.html">Moris JS</a></li>
-                                    <li><a href="echarts.html">ECharts</a></li>
-                                    <li><a href="other_charts.html">Other Charts</a></li>
-                                </ul>
-                            </li>
-                            <li><a><i class="fa fa-clone"></i>Layouts <span class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu">
-                                    <li><a href="fixed_sidebar.html">Fixed Sidebar</a></li>
-                                    <li><a href="fixed_footer.html">Fixed Footer</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="menu_section">
-                        <h3>Live On</h3>
-                        <ul class="nav side-menu">
-                            <li><a><i class="fa fa-bug"></i> Additional Pages <span
-                                            class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu">
-                                    <li><a href="e_commerce.html">E-commerce</a></li>
-                                    <li><a href="projects.html">Projects</a></li>
-                                    <li><a href="project_detail.html">Project Detail</a></li>
-                                    <li><a href="contacts.html">Contacts</a></li>
-                                    <li><a href="profile.html">Profile</a></li>
-                                </ul>
-                            </li>
-                            <li><a><i class="fa fa-windows"></i> Extras <span class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu">
-                                    <li><a href="page_403.html">403 Error</a></li>
-                                    <li><a href="page_404.html">404 Error</a></li>
-                                    <li><a href="page_500.html">500 Error</a></li>
-                                    <li><a href="plain_page.html">Plain Page</a></li>
-                                    <li><a href="login.html">Login Page</a></li>
-                                    <li><a href="pricing_tables.html">Pricing Tables</a></li>
-                                </ul>
-                            </li>
-                            <li><a><i class="fa fa-sitemap"></i> Multilevel Menu <span
-                                            class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu">
-                                    <li><a href="#level1_1">Level One</a>
-                                    <li><a>Level One<span class="fa fa-chevron-down"></span></a>
+                                    <li><a>Data Master <span class="fa fa-chevron-down"></span></a>
                                         <ul class="nav child_menu">
-                                            <li class="sub_menu"><a href="level2.html">Level Two</a>
+                                            <li><a href="{{route('home-admin')}}">Admins</a></li>
+                                            <li><a>Users <span class="fa fa-chevron-down"></span></a>
+                                                <ul class="nav child_menu">
+                                                    <li><a href="{{route('home-admin')}}">Agencies</a></li>
+                                                    <li><a href="{{route('home-admin')}}">Seekers</a></li>
+                                                </ul>
                                             </li>
-                                            <li><a href="#level2_1">Level Two</a>
+                                            <li><a>Requirements <span class="fa fa-chevron-down"></span></a>
+                                                <ul class="nav child_menu">
+                                                    <li><a href="{{route('home-admin')}}">Education Degrees</a></li>
+                                                    <li><a href="{{route('home-admin')}}">Education Majors</a></li>
+                                                    <li><a href="{{route('home-admin')}}">Industry</a></li>
+                                                    <li><a href="{{route('home-admin')}}">Job Function</a></li>
+                                                    <li><a href="{{route('home-admin')}}">Job Levels</a></li>
+                                                    <li><a href="{{route('home-admin')}}">Job Types</a></li>
+                                                    <li><a href="{{route('home-admin')}}">Salaries</a></li>
+                                                </ul>
                                             </li>
-                                            <li><a href="#level2_2">Level Two</a>
+                                            <li><a>Web Contents <span class="fa fa-chevron-down"></span></a>
+                                                <ul class="nav child_menu">
+                                                    <li><a href="{{route('home-admin')}}">Carousels</a></li>
+                                                    <li><a href="{{route('home-admin')}}">Payment Category</a></li>
+                                                    <li><a href="{{route('home-admin')}}">Payment Method</a></li>
+                                                    <li><a href="{{route('home-admin')}}">Plans</a></li>
+                                                    <li><a href="{{route('home-admin')}}">Nations</a></li>
+                                                    <li><a href="{{route('home-admin')}}">Provinces</a></li>
+                                                    <li><a href="{{route('home-admin')}}">Cities</a></li>
+                                                </ul>
                                             </li>
                                         </ul>
                                     </li>
-                                    <li><a href="#level1_2">Level One</a>
+                                    <li><a>Data Agencies <span class="fa fa-chevron-down"></span></a>
+                                        <ul class="nav child_menu">
+                                            <li><a href="{{route('home-admin')}}">Galleries</a></li>
+                                            <li><a href="{{route('home-admin')}}">Job Vacancies</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a>Data Seekers <span class="fa fa-chevron-down"></span></a>
+                                        <ul class="nav child_menu">
+                                            <li><a href="{{route('home-admin')}}">Attachments</a></li>
+                                            <li><a href="{{route('home-admin')}}">Educations History</a></li>
+                                            <li><a href="{{route('home-admin')}}">Language Skills</a></li>
+                                            <li><a href="{{route('home-admin')}}">Organizations</a></li>
+                                            <li><a href="{{route('home-admin')}}">Other Skills</a></li>
+                                            <li><a href="{{route('home-admin')}}">Trainings/Certifications</a></li>
+                                            <li><a href="{{route('home-admin')}}">Work Experiences</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a>Data Transaction <span class="fa fa-chevron-down"></span></a>
+                                        <ul class="nav child_menu">
+                                            <li><a href="{{route('home-admin')}}">Applications</a></li>
+                                            <li><a href="{{route('home-admin')}}">Favorite Agencies</a></li>
+                                            <li><a href="{{route('home-admin')}}">Job Postings</a></li>
+                                            <li><a href="{{route('home-admin')}}">Job Invitations</a></li>
+                                        </ul>
                                     </li>
                                 </ul>
                             </li>
-                            <li><a href="javascript:void(0)"><i class="fa fa-laptop"></i> Landing Page <span
-                                            class="label label-success pull-right">Coming Soon</span></a></li>
                         </ul>
                     </div>
 
