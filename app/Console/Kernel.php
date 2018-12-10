3be56209eed3ff0aac56b940515e6fe570cd9efe
Fiqy Ainuzzaqy <fiqy_a@icloud.com>
2018-12-10 17:05:23 +0700

    update on cron job/task scheduling: - abortingPayment admin_id by root [changed] - inactive vacancy based on active_period [added]

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\Agencies\abortingPayment::class,
-        Commands\Seekers\inactiveVacancy::class,
+        Commands\Agencies\inactiveVacancy::class,
         Commands\deleteInactiveUser::class,
     ];
 
