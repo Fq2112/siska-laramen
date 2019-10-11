38617502cbe65350da71f1e914e69f01ba19e7bb
ilham_puji <ilham.puji100@gmail.com>
2019-10-11 17:47:57 +0700

    Minor Update authcontroller API

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -286,22 +286,30 @@ class AuthController extends Controller
      */
     public function me()
     {
-        $user = User::findOrFail($this->guard()->user()->id)->toArray();
-        if ($user['ava'] == "seeker.png" || $user['ava'] == "") {
-            $filename = asset('images/seeker.png');
-        } else {
-            $filename = asset('storage/users/' . $user['ava']);
+        try{
+            JWTAuth::parseToken()->authenticate();
+            $user = User::findOrFail($this->guard()->user()->id)->toArray();
+
+            if ($user['ava'] == "seeker.png" || $user['ava'] == "") {
+                $filename = asset('images/seeker.png');
+            } else {
+                $filename = asset('storage/users/' . $user['ava']);
+            }
+            $seeker = Seekers::where('user_id', $user['id'])->first();
+
+            $acc = Accepting::where('seeker_id', $seeker['id'])
+                ->where('isBookmark',true)->get()->toArray();
+            $invite = Invitation::where('seeker_id', $seeker['id'])->get()->toArray();
+
+            $notif['count'] = array('invite' => count($invite), 'bookmarks' => count($acc));
+            $ava['user'] = array('ava' => $filename, 'name' => $user['name']);
+            $array = array_replace($user, $ava,$notif);
+            return response()->json($array);
+        }catch (\Exception $e){
+            return response()->json([
+                "error"=>$e
+            ],500);
         }
-        $seeker = Seekers::where('user_id', $user['id'])->first();
-
-        $acc = Accepting::where('seeker_id', $seeker['id'])
-            ->where('isBookmark',true)->get()->toArray();
-        $invite = Invitation::where('seeker_id', $seeker['id'])->get()->toArray();
-
-        $notif['count'] = array('invite' => count($invite), 'bookmarks' => count($acc));
-        $ava['user'] = array('ava' => $filename, 'name' => $user['name']);
-        $array = array_replace($user, $ava,$notif);
-        return response()->json($array);
     }
 
     /**
--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'ilham.puji100@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Ilham Puji'),
     ],
 
     /*
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('MAIL_USERNAME','ilham.puji100@gmail.com'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('MAIL_PASSWORD','lord1234puji'),
 
     /*
     |--------------------------------------------------------------------------
